9afd8fc48bbd05a01e93bf86d261192f57b56679
Ashirvad kar pathak <59271649+ashirvadkp33@users.noreply.github.com>
2023-11-05 09:56:34 +0530

    Delete Day1 directory

--- Day1/No.OfNodesAtNLevel.cpp
+++
@@ -1,6 +0,0 @@
-//https://www.codingninjas.com/studio/problems/number-of-nodes_8162204?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
-
-int numberOfNodes(int n){
-    // Write your code here.
-    return pow(2,n-1);
-}
